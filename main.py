5cc89ffbb0854382bfb1b7e83310edf952a06ccf
ejfisc <ejfisc@protonmail.com>
2022-04-10 01:49:59 -0500

    finished evaluation of system commands

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import os, time, map_data
 users = {'Ethan': 'Fischer', 'user': 'pass', 'logi': 'tech', 'test': 'python'}
 
 # robots dictionary - name: (start, end)
-robots = {'r1': ('start', 'end'), 'r2': ('north', 'south'), 'r3': ('east', 'west')}
+robots = {}
 
 # locations dictionary - name: (x, y)
-locations = {'nw': (0, 0), 'ne': (15, 0), 'se': (15, 15), 'sw': (0, 15)}
+locations = {}
+
+# paths list - (start, end)
+paths = [] 
 
 # create the map
 rows, cols = (15, 15)
@@ -48,7 +51,7 @@ def simulation():
     # TODO: draw the map after simulation
     input('press enter to exit')
     return
-    
+
 def command_line():
     os.system('cls||clear')
     print('COMMAND LINE')
@@ -60,55 +63,144 @@ def command_line():
                 os.system('cls||clear')
             case 'exit':
                 return
+            case 'print':
+                if len(command) != 2:
+                    print('Invalid Number of Arguments, use "help" for more info.\n')
+                    continue
+                match command[1]:
+                    case 'locations':
+                        print('\nLocations:')
+                        for k, v in locations.items():
+                            print(f'{k}: ({v[0]}, {v[1]})')
+                        print()
+                    case 'robots':
+                        print('\nRobots:')
+                        for k, v in robots.items():
+                            print(f'{k}: ({v[0]}, {v[1]})')
+                        print()
+                    case 'paths':
+                        print('\nPaths:')
+                        for path in paths:
+                            print(f'({path[0]}, {path[1]})')
+                        print()
             case 'location':
                 if len(command) != 4:
-                    print('Invalid Command')
+                    print('Invalid Number of Arguments, use "help" for more info.\n')
                     continue
+                if command[1] in robots:
+                    print('Locations cannot have the same name as robots.\n')
+                    continue
+                elif command[1] in locations:
+                    print(f'{command[1]} already exists.\n')
+                    continue
+                elif int(command[2]) < 0 or int(command[2]) > cols:
+                    print(f'{command[2]} is outside the map limits: {cols} x {rows}\n')
+                elif int(command[3]) < 0 or int(command[3]) > rows:
+                    print(f'{command[3]} is outside the map limits: {cols} x {rows}\n')
+                elif (command[2], command[3]) in locations.values():
+                    for k, v in locations.items():
+                       if v == (command[2], command[3]):
+                            locations.pop(k)
+                            break
+                    locations.update({command[1]: (command[2], command[3])})
+                    print(f'Location at ({command[2]}, {command[3]}) updated to {command[1]}.\n')
+                else:
+                    locations.update({command[1]: (command[2], command[3])})
+                    print(f'Location {command[1]} created at ({command[2]}, {command[3]}).\n')
             case 'robot':
                 if len(command) != 4:
-                    print('Invalid Command')
+                    print('Invalid Number of Arguments, use "help" for more info.\n')
                     continue
+                if command[1] in locations:
+                    print('Robots cannot have the same name as locations.\n')
+                elif command[1] in robots:
+                    print(f'{command[1]} already exists.\n')
+                elif command[2] not in locations or command[3] not in locations:
+                    print('Invalid location.\n')
+                else:
+                    robots.update({command[1]: (command[2], command[3])})
+                    print(f'Robot {command[1]} has been created at {command[2]} and will move to {command[3]}.\n')
             case 'path':
-                pass
+                if len(command) != 3:
+                    print('Invalid Number of Arguments, use "help" for more info.\n')
+                    continue
+                if command[1] not in locations:
+                    print(f'{command[1]} is not a location on this map.\n')
+                elif command[2] not in locations:
+                    print(f'{command[2]} is not a location on this map.\n')
+                elif (command[1], command[2]) in paths:
+                    print(f'The path ({command[1]}, {command[2]}) already exists.\n')
+                else:
+                    paths.append((command[1], command[2]))
+                    print(f'The path ({command[1]}, {command[2]}) was successfully created.\n')
             case 'remove':
                 if len(command) < 2 or len(command) > 3:
-                    print('Invalid Command')
+                    print('Invalid Number of Arguments, use "help" for more info.\n')
                     continue
                 elif len(command) == 2:
                     if command[1] in robots:
                         robots.pop(command[1])
-                        print(f'Robot {command[1]} successfully removed.')
+                        print(f'Robot {command[1]} successfully removed.\n')
                         print('\nRobots:')
                         for k, v in robots.items():
                             print(f'{k}: {v}')
                         print()
                     elif command[1] in locations:
                         locations.pop(command[1])
-                        print(f'Location {command[1]} successfully removed.')
+                        print(f'Location {command[1]} successfully removed.\n')
                         print('\nLocations:')
                         for k, v in locations.items():
                             print(f'{k}: {v}')
                         print()
+                        # remove any instances of the location in paths
+                        for path in paths:
+                            if command[1] in path:
+                                paths.remove(path)
+                                print(f'The path ({path[0]}, {path[1]}) which contains {command[1]} has been removed.')
+                        print()
                     else:
-                        print(f'{command[1]} is not a robot or location on this map.')
+                        print(f'{command[1]} is not a robot or location on this map.\n')
                         continue
+                elif len(command) == 3:
+                    if command[1] not in locations:
+                        print(f'{command[1]} is not a location on this map.\n')
+                    elif command[2] not in locations:
+                        print(f'{command[2]} is not a location on this map.\n')
+                    else:
+                        temp_path = (command[1], command[2])
+                        paths.remove(temp_path)
+                        print(f'Path ({command[1]}, {command[2]}) successfully removed.\n')
+                        print('\nPaths:')
+                        for path in paths:
+                            print(f'({path[0]}, {path[1]})')
+                        print()
             case _:
-                print('Invalid Command')
+                print('Invalid Command, use "help" for more info.\n')
 
 def command_list():
     os.system('cls||clear')
-    print('SYSTEM COMMANDS\n')
-    print('location name x y - Creates a new location named with the name "name" at (x, y) on the map\n\
-           \t\tat the location (x, y). If the location already exists, the system does nothing. Locations\n\
-           \t\tand robots cannot have the same names.\n')
-    print('remove name - Removes the given robot or location, if it exists.\n')
-    print('remove start end - Removes the given path, if it exists.\n')
-    print('path start end - Creates a directed path from start location to end location, if both\n\
-           \t    locations exist.\n')
-    print('robot name start end - Creates a robot with the name "name" and put it on the map at start\n\
-           \t\t   with the destination at end.\n')
-    print('exit - Exits the system command line.\n')
-    print('help - Prints out the command list.\n')
+    print('SYSTEM COMMANDS')
+    print('''
+location <name> <x> <y> - Creates a new location named with the name "name" at (x, y) on the map
+                          at the location (x, y). If the location already exists, the system does nothing. Locations
+                          and robots cannot have the same names.
+
+remove <name> - Removes the given robot or location, if it exists.
+             
+remove <start> <end> - Removes the given path, if it exists.
+             
+path <start> <end> - Creates a directed path from start location to end location, if both locations exist.
+             
+robot <name> <start> <end> - Creates a robot with the name "name" and put it on the map at start
+                             with the destination at end.
+
+print <list> - Prints the given list to the screen. <list> could be "locations" or "robots" or "paths"
+
+exit - Exits the system command line.
+             
+help - Prints out the command list.
+
+''')
     input('press enter to exit')
     return
 
@@ -140,5 +232,4 @@ def main():
                 print('Invalid option, try again.')
                 time.sleep(1)
     
-       
 command_line()
--- robot.py
+++
@@ -1,9 +0,0 @@
-#! python3
-# robot.py - robot class
-
-class Robot:
-    def __init__(self, name, start, destination):
-        self.name = name
-        self.start = start
-        self.destination = destination
-        
